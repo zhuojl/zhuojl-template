7067411554534b8423fc0cda8b82e21938df648d
zhuojunliang <junliang.zhuo@faw-vw.com>
2023-07-28 21:40:04 +0800

    feat(): 删除extension-starter模块，有需要再研究源码

--- components/component-extension-starter/src/test/java/com/zjl/component/extension/register/SomeExtPt.java
+++
@@ -1,8 +0,0 @@
-package com.zjl.component.extension.register;
-
-import com.zjl.component.extension.ExtensionPointI;
-
-public interface SomeExtPt extends ExtensionPointI {
-    
-    public void doSomeThing();
-}
