9080c59ae7b069b1b6473ac8972fe56a40ab0894
zhuojunliang <junliang.zhuo@faw-vw.com>
2023-08-02 21:27:30 +0800

    feat(): 业务测试，从json中加载基础数据用于测试，避免在代码中人工大量构造数据

--- archetypes/archetype-web/archetype-web-domain-impl/src/test/java/com/zjl/archetype/web/domain/customer/service/CustomerServiceTest.java
+++ archetypes/archetype-web/archetype-web-domain-impl/src/test/java/com/zjl/archetype/web/domain/customer/service/CustomerServiceTest.java
@@ -4,6 +4,7 @@ import com.zjl.archetype.web.domain.customer.Customer;
 import com.zjl.archetype.web.infra.dao.CustomerDO;
 import com.zjl.archetype.web.infra.dao.CustomerMapper;
 import com.zjl.archetype.web.infra.event.EventPublisher;
+import com.zjl.archetype.web.infra.util.JsonUtil;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
 
 //@ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class) // 和 infra 不一致？ infra使用 ExtendWith 方式，
@@ -24,10 +27,15 @@ public class CustomerServiceTest {
     private EventPublisher eventPublisher;
 
     @Test
-    public void testGetByById() {
-        Mockito.when(customerMapper.getByCustomerId(Mockito.anyString())).thenReturn(new CustomerDO());
+    public void testGetByById() throws IOException {
+        CustomerDO customerDO = JsonUtil.parseJsonFromClassPathFile("test/domain/customer/CustomerDO.json", CustomerDO.class);
+        Mockito.when(customerMapper.getByCustomerId("test")).thenReturn(customerDO);
         Customer customer = customerService.getByById("test");
         Assert.assertNotNull(customer);
+        Assert.assertEquals(customerDO.getCustomerId(), customer.getCustomerId());
+
+        Customer customer2 = customerService.getByById("test1");
+        Assert.assertNull(customer2);
     }
 
     @Test
