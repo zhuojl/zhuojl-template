d562e92c13b55fb8d420a3c72610077e2b7478f5
zhuojunliang <junliang.zhuo@faw-vw.com>
2023-12-12 20:22:38 +0800

    feat(): feign 测试

--- archetypes/archetype-web/archetype-web-infra/src/main/java/com/zjl/archetype/web/infra/feign/FeignTest.java
+++ archetypes/archetype-web/archetype-web-infra/src/main/java/com/zjl/archetype/web/infra/feign/FeignTest.java
@@ -7,6 +7,7 @@ import com.zjl.component.feign.FeignConfiguration;
 import com.zjl.archetype.web.infra.dao.CustomerDO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,6 +23,9 @@ public interface FeignTest {
 
     @PostMapping(value = "internal/testFeign2?aa=测试", consumes = "application/x-www-form-urlencoded")
     CustomerDO test2(String param);
+
+    @PostMapping(value = "internal/testFeign22?aa=测试", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    String test22(String param);
 
     /**
      *  form 表单的场景 特殊处理，因为tomcat对form表单有特殊处理，不能用@RequestBody注解（接收参数也不能用这个注解）
--- archetypes/archetype-web/bootstrap/src/main/java/com/zjl/archetype/web/TempFeignTestController.java
+++ archetypes/archetype-web/bootstrap/src/main/java/com/zjl/archetype/web/TempFeignTestController.java
@@ -1,31 +1,32 @@
 package com.zjl.archetype.web;
 
+import com.zjl.archetype.web.infra.dao.CustomerDO;
+import com.zjl.archetype.web.infra.feign.FeignTest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import com.zjl.archetype.web.infra.dao.CustomerDO;
-import com.zjl.archetype.web.infra.feign.FeignTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
+@Slf4j
 public class TempFeignTestController {
     @Autowired
     private FeignTest feignTest;
 
-    @PostMapping(value = "/internal/testFeign", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(value = "/internal/testFeign11", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public CustomerDO testFeignInternal() {
         CustomerDO customerDO = new CustomerDO();
         customerDO.setCustomerId("asdfadsf");
         return customerDO;
     }
-    @PostMapping(value = "/testFeign", consumes = "application/x-www-form-urlencoded")
+
+    @PostMapping(value = "/testFeign", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public CustomerDO testFeign() {
-        Map<String,String> param = new HashMap<>();
+        log.info("测试中文");
+        Map<String, String> param = new HashMap<>();
         param.put("aa", "bb");
         param.put("aaaa", "bbbb");
         feignTest.test(param);
@@ -35,35 +36,58 @@ public class TempFeignTestController {
         return customerDO;
     }
 
-    @PostMapping(value = "/internal/testFeign2", consumes = "application/x-www-form-urlencoded")
-    public CustomerDO testFeignInternal2(CustomerDO customerDO) {
-        return customerDO;
-    }
-    @PostMapping(value = "/testFeign2", consumes = "application/x-www-form-urlencoded")
-    public CustomerDO testFeign2(@ModelAttribute CustomerDO customerDO) {
-    // @RequestBody 不能与 application/x-www-form-urlencoded 混用，虽然http请求中参数在body。
-    // public CustomerDO testFeign2(@RequestBody CustomerDO customerDO) {
-        String bodyStr = "customerId=11111&memberId=耍到耍到";
-        feignTest.test2(bodyStr);
+    @GetMapping(value = "/testFeign44444")
+    public CustomerDO testFeign44444() {
+
+        //feignTest.test(JSONObject.toJSONString(param));
+        CustomerDO customerDO = new CustomerDO();
         customerDO.setCustomerId("asdfadsf");
         return customerDO;
     }
-    @PostMapping(value = "/testFeign3", consumes = "application/x-www-form-urlencoded")
+
+    @PostMapping(value = "/internal/testFeign2", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public CustomerDO testFeignInternal2(CustomerDO customerDO) throws InterruptedException {
+        return customerDO;
+    }
+
+    @PostMapping(value = "/testFeign2", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public CustomerDO testFeign2(@ModelAttribute CustomerDO customerDO) {
+        // @RequestBody 不能与 application/x-www-form-urlencoded 混用，虽然http请求中参数在body。
+        // public CustomerDO testFeign2(@RequestBody CustomerDO customerDO) {
+        String bodyStr = "customerId=11111&memberId=耍到耍到";
+        return feignTest.test2(bodyStr);
+    }
+
+    @PostMapping(value = "/internal/testFeign22", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String testFeignInternal22(CustomerDO customerDO) throws InterruptedException {
+        return "String";
+    }
+    @PostMapping(value = "/testFeign22", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String testFeign22(@ModelAttribute CustomerDO customerDO) {
+        // @RequestBody 不能与 application/x-www-form-urlencoded 混用，虽然http请求中参数在body。
+        // public CustomerDO testFeign2(@RequestBody CustomerDO customerDO) {
+        String bodyStr = "customerId=11111&memberId=耍到耍到";
+        return feignTest.test22(bodyStr);
+    }
+
+    @PostMapping(value = "/testFeign3", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public CustomerDO testFeign3(CustomerDO customerDO) {
-    // @RequestBody 不能与 application/x-www-form-urlencoded 混用，虽然http请求中参数在body。
-    // public CustomerDO testFeign2(@RequestBody CustomerDO customerDO) {
+        // @RequestBody 不能与 application/x-www-form-urlencoded 混用，虽然http请求中参数在body。
+        // public CustomerDO testFeign2(@RequestBody CustomerDO customerDO) {
         String bodyStr = "customerId=11111&memberId=memberId";
         feignTest.test3(customerDO);
         customerDO.setCustomerId("asdfadsf");
         return customerDO;
     }
-    @PostMapping(value = "/testFeign4", consumes = "application/x-www-form-urlencoded")
+
+    @PostMapping(value = "/testFeign4", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public CustomerDO testFeign4(CustomerDO customerDO) {
 
         return new CustomerDO();
     }
+
     @PostMapping(value = "/testFeign5", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String testFeign5(@RequestBody CustomerDO customerDO) {
-        return "1";
+    public CustomerDO testFeign5(@RequestBody CustomerDO customerDO) {
+        return customerDO;
     }
 }
