a210fbd11374cd2e6a5642bc250f5b92c928a933
zhuojunliang <junliang.zhuo@faw-vw.com>
2023-12-12 20:03:29 +0800

    feat(): feign自动解包装response对象

--- components/component-feign/src/main/java/com/zjl/component/feign/FeignConfiguration.java
+++ components/component-feign/src/main/java/com/zjl/component/feign/FeignConfiguration.java
@@ -1,17 +1,31 @@
 package com.zjl.component.feign;
 
+import com.zjl.component.common.model.Response;
+import com.zjl.component.feign.decode.FeignDecoder;
 import com.zjl.component.feign.decode.FeignErrorDecoder;
 import com.zjl.component.feign.interceptor.context.ContextInterceptor;
 import com.zjl.component.feign.interceptor.sign.Md5SignRequestInterceptor;
 import com.zjl.component.feign.interceptor.sign.SignRequestInterceptor;
 import com.zjl.component.feign.log.CustomFeignLoggerFactory;
 import feign.Logger;
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignLoggerFactory;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 
 public class FeignConfiguration {
 
+
+
+    @Bean
+    public Decoder feignDecoder(ObjectFactory<HttpMessageConverters> messageConverters) {
+        return new ResponseEntityDecoder(new FeignDecoder<>(Response.class, new SpringDecoder(messageConverters)));
+    }
 
     @Bean
     @ConditionalOnMissingBean(SignRequestInterceptor.class)
---
+++ components/component-feign/src/main/java/com/zjl/component/feign/decode/FeignDecoder.java
@@ -0,0 +1,70 @@
+package com.zjl.component.feign.decode;
+
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
+
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Objects;
+
+import feign.Response;
+import feign.codec.Decoder;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhuojl
+ */
+@Slf4j
+public class FeignDecoder<T extends com.zjl.component.common.model.Response> implements Decoder {
+    private final Class<T> resultCls;
+    private Decoder decoder;
+
+    public FeignDecoder(Class<T> resultCls, Decoder decoder) {
+        this.resultCls = resultCls;
+        this.decoder = decoder;
+    }
+
+    @Override
+    public Object decode(Response response, Type type) throws IOException {
+
+        // 如果是子类，则不再需要 后续的 ParameterizedTypeImpl 包装
+        if (isResultClsType(type)) {
+            return this.decoder.decode(response, type);
+        }
+
+        // XXX 注意第三个参数，如果 一个rpc的Response是内部类，需要多次包装，尽量避免吧。。
+        Type resultClsType = ParameterizedTypeImpl.make(com.zjl.component.common.model.Response.class, new Type[]{type}, null);
+
+        Object obj = decoder.decode(response, resultClsType);
+        T result = (T) obj;
+
+        if (Objects.isNull(result)) {
+            log.error("result is empty, must be sth wrong");
+            return null;
+        }
+
+        return result.getData();
+
+    }
+
+    /**
+     * type 是否是 resultCls 的子类
+     */
+    private boolean isResultClsType(Type type) {
+        // 当方法签名的返回参数没有使用泛型时，type是Class
+        if (type instanceof Class && resultCls.isAssignableFrom((Class) type)) {
+            return true;
+        }
+
+        // 当方法签名的返回参数是泛型时，type是ParameterizedType
+        if (type instanceof ParameterizedType) {
+            Type rawType = ((ParameterizedType) type).getRawType();
+            if (rawType instanceof Class && resultCls.isAssignableFrom((Class) rawType)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
