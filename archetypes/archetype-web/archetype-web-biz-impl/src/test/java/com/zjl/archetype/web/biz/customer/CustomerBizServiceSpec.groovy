2f8b839f2b344f56cfb6392793db9746c5198636
zhuojunliang <junliang.zhuo@faw-vw.com>
2023-12-16 15:45:36 +0800

    feat(): spock spock去除test，避免各个子model都依赖；使用groovy代替java 单元测试

---
+++ archetypes/archetype-web/archetype-web-biz-impl/src/test/java/com/zjl/archetype/web/biz/customer/CustomerBizServiceSpec.groovy
@@ -0,0 +1,47 @@
+package com.zjl.archetype.web.biz.customer
+
+import com.zjl.archetype.web.biz.customer.service.CustomerBizServiceImpl
+import com.zjl.archetype.web.client.customer.cmd.CustomerAddCmd
+import com.zjl.archetype.web.client.customer.dto.CustomerDTO
+import com.zjl.archetype.web.domain.customer.Customer
+import com.zjl.archetype.web.domain.customer.service.CustomerService
+import com.zjl.archetype.web.infra.exception.BizErrorEnum
+import com.zjl.component.common.exception.BadRequestException
+import spock.lang.Specification
+
+class CustomerBizServiceSpec extends Specification {
+    def customerService = Mock(CustomerService)
+    def customerBizService = new CustomerBizServiceImpl(customerService: customerService)
+
+    def testAddCustomer() {
+        given:
+        def customerDTO = new CustomerDTO(customerId: "DEFAULT_USER_ID", customerName: "DEFAULT_USER_NAME")
+        def customerAddCmd = new CustomerAddCmd(customer: customerDTO)
+
+        and:
+        customerService.getByCustomerName(_) >> null
+        customerService.addCustomer(_) >> 1
+
+        when:
+        def resp = customerBizService.addCustomer(customerAddCmd);
+        then:
+        1 == resp.getData()
+    }
+
+    def "testAddCustomer with exception"() {
+        given:
+        def customerDTO = new CustomerDTO(customerId: "DEFAULT_USER_ID", customerName: "DEFAULT_USER_NAME")
+        def customerAddCmd = new CustomerAddCmd(customer: customerDTO)
+
+        and:
+        customerService.getByCustomerName(_) >> new Customer()
+        customerService.addCustomer(_) >> 1
+
+        when:
+        def resp = customerBizService.addCustomer(customerAddCmd);
+
+        then:
+        def exception = thrown(BadRequestException)
+        exception.errCode == BizErrorEnum.CUSTOMER_NAME_REPEAT.errorCode()
+    }
+}
---
+++ archetypes/archetype-web/archetype-web-domain-impl/src/test/java/com/zjl/archetype/web/domain/customer/service/CustomerServiceSpec.groovy
@@ -0,0 +1,44 @@
+package com.zjl.archetype.web.domain.customer.service
+
+import com.zjl.archetype.web.domain.customer.Customer
+import com.zjl.archetype.web.infra.dao.customer.CustomerDO
+import com.zjl.archetype.web.infra.dao.customer.CustomerDao
+import com.zjl.archetype.web.infra.event.EventPublisher
+import com.zjl.archetype.web.infra.util.JsonUtil
+import org.junit.jupiter.api.Test
+import spock.lang.Specification
+
+class CustomerServiceSpec extends Specification {
+
+    def customerDao = Mock(CustomerDao)
+    def eventPublisher = Mock(EventPublisher)
+    def customerService = new CustomerServiceImpl(customerDao: customerDao, eventPublisher: eventPublisher)
+
+
+    def testGetByById()  {
+        given:
+        def customerDO = new CustomerDO(customerId:  "customerId", customerName: "customerName");
+        and:
+        customerDao.getByCustomerId(_) >> customerDO;
+
+        when:
+        def customer = customerService.getByCustomerId("test");
+
+        then:
+        customer.customerId == customerDO.customerId
+    }
+
+    def testAddCustomer() {
+        given:
+        def customer = new Customer(customerId:  "customerId", customerName: "customerName");
+        and:
+        customerDao.insert(_) >> 1;
+
+        when:
+        def customerId = customerService.addCustomer(customer);
+
+        then:
+        null == customerId
+    }
+
+}
